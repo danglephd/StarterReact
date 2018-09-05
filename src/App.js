b518c9206692f912d1d26e418cb2bf476bb230cf
danglephd <danglephd@gmail.com>
2018-09-05 10:06:39 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
       => Đã thử edit file ngay trên Github và xem web chạy được không
+      Kết quả không thấy cập nhật trên GUi
         </p>
       </div>
     );
