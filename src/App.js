e2972a53a29a2f5f618800163097be6240641717
danglephd <danglephd@gmail.com>
2018-09-05 10:09:54 +0700

    cập nhật

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-      => Đã thử edit file ngay trên Github và xem web chạy được không
+      =>1. Đã thử edit file ngay trên Github và xem web chạy được không
       Kết quả không thấy cập nhật trên GUi
+      =>2. cập nhật lại dưới local rồi commit và npm run deploy xem có thay đổi không
         </p>
       </div>
     );
